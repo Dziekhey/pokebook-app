29a18e7a75908a71f187735e04a4b0f1759bb0cb
Dziekhey <dakeysandra@gmail.com>
2024-05-17 18:18:26 +0000

    completed the about content

--- src/components/PokemondCard.jsx
+++ src/components/PokemondCard.jsx
@@ -1,24 +1,49 @@
-import React from "react";
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import React, { useState } from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DetailModal from "../pages/details/DetailModal";
 
 const PokemondCard = ({ pokemon }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
-    <div className="h-[200px] w-[260px] flex flex-col my-5 bg-white text-center items-center justify-start relative rounded-2xl group hover:h-auto hover:cursor-pointer">
-      <div className="h-auto w-full p-3 flex flex-col items-center">
-        <div className="bg-[#E1E1E1] h-28 w-full items-center justify-center rounded-lg relative">
-          <img
-            src={pokemon.sprites.other["official-artwork"].front_default}
-            alt="Pokemon"
-            className="w-44 h-44 object-contain absolute -bottom-1 left-1/2 transform -translate-x-1/2"
-          />
+      <div className="h-[225px] w-[260px] flex flex-col my-5 bg-white text-center items-center justify-start relative rounded-2xl group hover:h-auto hover:cursor-pointer">
+        <div className="h-auto w-full p-3 flex flex-col items-center">
+          <div className="bg-[#E1E1E1] h-28 w-full items-center justify-center rounded-lg relative">
+            <img
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt="Pokemon"
+              className="w-44 h-44 object-contain absolute -bottom-1 left-1/2 transform -translate-x-1/2"
+            />
+          </div>
+          <h1 className="mt-4 font-bold text-2xl">{pokemon.name}</h1>
+          <div className="flex">
+            {pokemon.types.map((poketype) => (
+              <h3
+                key={poketype.type.name}
+                className="mx-2 mt-3 px-3 py-1 rounded-2xl bg-[#E1E1E1]"
+              >
+                {poketype.type.name}
+              </h3>
+            ))}
+          </div>
+
+          <div className="opacity-0 group-hover:opacity-100 mt-4">
+            <button
+              className="bg-primary text-white rounded-xl px-3 py-3 flex justify-between"
+              onClick={handleOpen}
+            >
+              View Pokémon
+              <span className="flex pl-20 justify-end">
+                <VisibilityIcon />
+              </span>
+            </button>
+          </div>
         </div>
-        <h1 className="mt-4 font-bold text-2xl">{pokemon.name}</h1>
-        <div className="opacity-0 group-hover:opacity-100 mt-4">
-          <button className="bg-primary text-white rounded-xl px-3 py-3 flex justify-between">View Pokémon<span className="flex pl-20 justify-end"><VisibilityIcon /></span></button>
-        </div>
+        <DetailModal open={open} handleClose={handleClose} />
       </div>
-    </div>
     </>
   );
 };
---
+++ src/pages/details/DetailModal.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import sample from "../../assets/logo.png";
+import About from "../../components/About";
+import Stats from "../../components/Stats";
+import Similar from "../../components/Similar";
+import DetailTabs from "../../components/DetailTabs";
+
+const style = {
+  position: "absolute",
+  left: "55%",
+  width: 575,
+  height: 650,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 2,
+};
+
+const DetailModal = ({ open, handleClose }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-40 w-full items-center justify-center rounded-lg relative">
+            <ArrowBackIcon
+              sx={{ width: "40px", height: "40px" }}
+              className="absolute bg-white p-2 m-2 rounded-lg"
+            />
+            <img
+              src={sample}
+              alt="Pokemon"
+              className="w-64 h-64 object-contain absolute left-1/2 transform -translate-x-1/2"
+            />
+          </div>
+          <div className="">
+            <h1 className="flex justify-center mt-14 font-bold text-4xl">
+              Ivysaur
+            </h1>
+            <div className="pb-5">
+              <h3 className="flex justify-center rounded-2xl bg-[#E1E1E1] my-3">
+                type
+              </h3>
+            </div>
+          </div>
+          <div className="flex justify-center w-[510px] h-[215px] mt-2">
+            {selectedTab === 0 && <About />}
+            {selectedTab === 1 && <Stats />}
+            {selectedTab === 2 && <Similar />}
+          </div>
+          <div className="flex justify-center">
+            <DetailTabs selectedTab={selectedTab} handleChange={handleChange} />
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default DetailModal;
